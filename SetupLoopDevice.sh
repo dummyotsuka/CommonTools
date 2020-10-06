40c8ce43284ca79ee16af86ae443eae7046fcb45
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-10-06 14:06:16 +0900

    Update SetupLoopDevice.sh

--- SetupLoopDevice.sh
+++ SetupLoopDevice.sh
@@ -5,5 +5,7 @@
 # fdisk /dev/loop11
 # partprobe /dev/loop11
 
+# if losetup | grep /dev/loop11; then echo ok; else echo ng; fi
 
-
+# This is the answer.
+# if losetup | grep /dev/loop12; then echo /dev/loop12のディスクは研修用に準備済みです; else dd if=/dev/zero of=disk12.img bs=1024 count=300000; losetup /dev/loop12 /root/disk12.img; echo /dev/loop12を再構築しました; fi
