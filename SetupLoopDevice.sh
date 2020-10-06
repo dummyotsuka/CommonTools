8726d1ab8cc19256f0e64d5e2f8d1d0f9071c15b
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-10-06 14:26:43 +0900

    Update SetupLoopDevice.sh

--- SetupLoopDevice.sh
+++ SetupLoopDevice.sh
@@ -12,4 +12,6 @@
 
 # function SetupLoopDevice() { if losetup | grep /dev/loop12; then echo /dev/loop12のディスクは研修用に準備済みです; else dd if=/dev/zero of=disk12.img bs=1024 count=300000; losetup /dev/loop12 /root/disk12.img; echo /dev/loop12を再構築しました; fi; }
 
+# function SetupLoopDevice3() { echo $1 ; if losetup | grep /dev/loop$1; then echo "/dev/loop$1のディスクは研修用に準備済みです"; else dd if=/dev/zero of=/root/disk$1.img bs=1024 count=300000; losetup /dev/loop$1 /root/disk$1.img; echo "/dev/loop$1を再構築しました"; fi; }
 
+function SetupLoopDevice() { if losetup | grep /dev/loop$1; then echo "/dev/loop$1のディスクは研修用に準備済みです"; else dd if=/dev/zero of=/root/disk$1.img bs=1024 count=300000; losetup /dev/loop$1 /root/disk$1.img; echo "/dev/loop$1を再構築しました"; fi; }
